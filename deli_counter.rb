bcf00e4992e9dec078d3a67cbe805e3162e10cd8
joseph.chesir <joseph.chesir@mail.yu.edu>
2018-10-30 20:37:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,11 +19,12 @@ def line(katz_deli)
 end 
 
 
-
-def take_a_number(katz_deli,name)
-   katz_deli.push(name)
+$newnumber = 0, 
+def take_a_number(katz_deli,)
+  $newnumber + 1
+   katz_deli.push($newnumber+1)
   
-    puts "Welcome, #{name}. You are number #{katz_deli.length} in line." 
+    puts ". You are number #{katz_deli.length} in line." 
 
      
 end 
